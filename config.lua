992253bc9341cde239ca33862b3bef9c8acadc8a
nerored <nero_stellar@icloud.com>
2022-06-24 23:18:29 +0800

    config setting

--- config.lua
+++ config.lua
@@ -67,16 +67,16 @@ lvim.keys.normal_mode["<leader>DS"] = '<Cmd>lua require("dapui").open()<CR>")'
 -- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
-lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["t"] = {
-	name = "+Trouble",
-	r = { "<cmd>Trouble lsp_references<cr>", "References" },
-	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
-}
+--lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+--lvim.builtin.which_key.mappings["t"] = {
+--	name = "+Trouble",
+--	r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+--	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
+--}
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
